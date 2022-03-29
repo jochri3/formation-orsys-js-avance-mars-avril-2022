eecd9d2009a7bce825d58c6bc6ece0bea9da75ff
Christian Lisangola <jochri3@gmail.com>
2022-03-29 12:38:00 +0300

    2_template string

---
+++ 1_es6/2_template_strings/app.js
@@ -0,0 +1,32 @@
+const nom = "jean";
+const prenom = "Toto";
+const age = 45;
+const paysNaissance = "France";
+console.log(
+  "Vous vous appelez " +
+    prenom +
+    " " +
+    nom +
+    ",vous êtes nés en " +
+    paysNaissance +
+    ".Et vous avez " +
+    age +
+    " ans"
+);
+console.log(
+  `Vous vous appelez ${prenom} ${nom},vous êtes nés en ${paysNaissance}.Et vous avez ${age} ans`
+);
+
+//
+const nombre1 = 67;
+const nombre2 = 56;
+
+console.log(nombre1 + " + " + nombre2 + " = " + (nombre1 + nombre2) + "");
+
+console.log(`${nombre1} + ${nombre2} = ${nombre1 + nombre2}`);
+
+const phrase = `Je suis 
+développeur 
+mobile et desktop`;
+
+console.log(phrase);
