7034082683e28d94f58067c0b9fdd4378bcdd6d8
Christian Lisangola <jochri3@gmail.com>
2022-04-01 10:54:26 +0300

    add media factory

--- 6_design_patterns/factory.js
+++ 6_design_patterns/factory.js
@@ -1,6 +1,8 @@
 const CONSTANTES = Object.freeze({
   DEVELOPER: "developer",
   TESTER: "tester",
+  IMAGE: "image",
+  VIDEO: "video",
 });
 
 class Personne {
@@ -45,3 +47,44 @@ employees.push(
 employees.push(employeeFactory.create("Xavier", CONSTANTES.TESTER));
 employees.push(employeeFactory.create("Anass L", CONSTANTES.TESTER));
 employees.push(employeeFactory.create("Damien Le Guen", CONSTANTES.DEVELOPER));
+
+// Medias
+
+const medias = [
+  { id: 1, author: "Jean", title: "Toto", image: "toto.jpg" },
+  { id: 2, author: "Xavier", title: "Ma photo", image: "xavier.jpg" },
+  { id: 3, author: "Chris", title: "Ma video", video: "moi.mp4" },
+];
+class Media {
+  constructor(id, title, author) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+  }
+}
+
+class Image extends Media {
+  constructor(id, title, author, image) {
+    super(id, title, author);
+    // this.image = image;
+    this.mediaURL = image;
+  }
+}
+
+class Video extends Media {
+  constructor(id, title, author, video) {
+    super(id, title, author);
+    // this.video = video;
+    this.mediaURL = video;
+  }
+}
+
+class MediaFactory {
+  create(media) {
+    if (media[CONSTANTES.IMAGE]) {
+      return new Image(media.id, media.title, media.author, media.image);
+    } else {
+      return new Video(media.id, media.title, media.author, media.video);
+    }
+  }
+}
