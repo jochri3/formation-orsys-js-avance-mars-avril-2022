6a3fa266b74d96cc1a047884a137e9448989e0eb
Christian Lisangola <jochri3@gmail.com>
2022-04-01 15:28:38 +0300

    express:post request

---
+++ 7_node_js/app.js
@@ -0,0 +1,13 @@
+const os = require("os");
+
+// cpus : nombre de coeurs du CPU
+console.log(os.cpus());
+
+// freemem
+console.log(os.freemem());
+
+// totalmem
+console.log(os.totalmem());
+
+// hostmane
+console.log(os.hostname());
---
+++ 8_express/src/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const contacts = require("./database");
+const { nanoid } = require("nanoid");
+const { application } = require("express");
+
+const App = express();
+
+const PORT = 3000;
+
+App.use(express.json());
+
+App.get("/api/contacts", (request, response) => {
+  response.send(contacts);
+});
+
+App.get("/api/contacts/:id", (request, response) => {
+  const id = request.params.id;
+  const contact = contacts.find((contact) => contact._id === id);
+  if (!contact) {
+    return response.status(404).send({ message: "Not found" });
+  }
+  return response.send(contact);
+});
+
+App.post("/api/contacts", (request, response) => {
+  const data = request.body;
+  contacts.push({ ...data, _id: nanoid() });
+  response.status(201).send({ message: "Resource created successfully" });
+
+});
+
+App.listen(PORT, () => {
+  console.log(`Le serveur écoute sur le port -- ${PORT}`);
+});
